1fae78ab588552bacd30ce7daf1c85c1a4516c10
João Marcelo Souza <joaomarcelo9393@gmail.com>
2017-08-28 11:46:25 -0300

    Document code

--- popup.js
+++ popup.js
@@ -1,9 +1,12 @@
+// Shortcut to the console.log function.
 var log = chrome.extension.getBackgroundPage().console.log
 
+// Need to wait for window to load before adding events to elements.
 window.onload = () => {
   var screenshotImage = document.querySelector('#screenshot-image')
   var takeScreenshotButton = document.querySelector('#take-screenshot')
 
+  // Take screenshot and show on extension's window.
   takeScreenshotButton.onclick = () => {
     chrome.tabs.captureVisibleTab(null, {}, function (imageURL) {
        screenshotImage.src = imageURL
